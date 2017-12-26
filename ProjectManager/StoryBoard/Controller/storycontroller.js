1d1c581ab62863e7f33c778c67a9987920f7e98d
Akrem <akrem1st@hotmail.com>
2017-12-26 16:08:21 -0500

    project manager
    second to Jira

---
+++ ProjectManager/StoryBoard/Controller/storycontroller.js
@@ -0,0 +1,10 @@
+﻿angular.module('storyApp')
+    .controller('StoryController', function (STORY_TYPES, StoryModel) {
+        var sc = this;
+
+        sc.storyTypes = STORY_TYPES;
+
+        sc.stories = StoryModel.stories;
+
+        sc.updatingStory = angular.copy(sc.stories[0]);
+    });
---
+++ ProjectManager/StoryBoard/Model/StoryModel.js
@@ -0,0 +1,27 @@
+﻿angular.module('storyApp')
+    .value('STORY_TYPES', [
+        { name: 'Design' },
+        { name: 'Dev' },
+        { name: 'QA' },
+        { name: 'Release' }
+    ]
+    )
+    .service('StoryModel', function (STORY_TYPES) {
+        var story = this;
+        story.stories = [
+            {
+                id: 101,
+                title: "story 1",
+                description: "first story",
+                type: STORY_TYPES[0],
+                created: new Date('2017/12/01')
+            },
+            {
+                id: 102,
+                title: "story 2",
+                description: "first story",
+                type: STORY_TYPES[2],
+                created: new Date('2017/12/02')
+            }
+        ];
+    });
